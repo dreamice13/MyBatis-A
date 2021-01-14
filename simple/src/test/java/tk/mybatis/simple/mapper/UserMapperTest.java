f42167ea9bd4bed01105553629670c1fa13dfc6e
Dreamice <dreamice13@foxmail.com>
2021-01-14 19:10:21 +0800

    initialization

---
+++ simple/src/test/java/tk/mybatis/simple/mapper/UserMapperTest.java
@@ -0,0 +1,55 @@
+package tk.mybatis.simple.mapper;
+
+import java.util.List;
+
+import javax.management.relation.Role;
+
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Assert;
+import org.junit.Test;
+
+import tk.mybatis.simple.model.SysRole;
+import tk.mybatis.simple.model.SysUser;
+
+public class UserMapperTest extends BaseMapperTest {
+	
+	@Test
+	public void testSelectById() {
+		// 获取sqlSession
+		SqlSession sqlSession = getSqlSession();
+		try {
+			UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+			SysUser user = userMapper.selectById((long) 1);
+			Assert.assertNotNull("用户查询结果为空", user);
+			Assert.assertEquals("admin", user.getUserName());
+		} finally {
+			sqlSession.close();
+		}
+	}
+
+	@Test
+	public void testSelectAll() {
+		SqlSession sqlSession = getSqlSession();
+		try {
+			UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+			List<SysUser> userList = userMapper.selectAll();
+			Assert.assertNotNull(userList);
+			Assert.assertTrue(userList.size() > 0 );
+		}finally {
+			sqlSession.close();
+		}
+	}
+	
+	@Test
+	public void testSelectRolesByUserId() {
+		SqlSession sqlSession = getSqlSession();
+		try {
+			UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+			List<SysRole> roleList = userMapper.selectRolesByUserId((long) 1);
+			Assert.assertNotNull(roleList);
+			Assert.assertTrue(roleList.size() > 0 );
+		}finally {
+			sqlSession.close();
+		}
+	}
+}
